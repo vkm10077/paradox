71fa9a394a3fa6ffd031a15961ca619e996f8571
vkm10077 <vkm10077@gmail.com>
2026-07-03 12:20:11 +0530

    Update nifty500.py

--- nifty500.py
+++ nifty500.py
@@ -1,11 +1,4 @@
-import pandas as pd
 
-URL = "https://nsearchives.nseindia.com/content/indices/ind_nifty500list.csv"
-
-try:
-    df = pd.read_csv(URL)
-    NIFTY500 = ["NSE:" + s + "-EQ" for s in df["Symbol"].dropna().tolist()]
-except Exception:
     NIFTY500 = [
         "NSE:RELIANCE-EQ",
         "NSE:TCS-EQ",

8ac84015a1f3ad688d328991ab59da40029ac07f
vkm10077 <vkm10077@gmail.com>
2026-07-03 12:48:03 +0530

    Replace scanner_results initialization with scan_nifty500 call

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def dashboard():
                     "prev_close": v.get("prev_close_price", 0),
                 })
                 
-    scanner_results = []
+    scanner_results = scan_nifty500(fyers)
     
     selected_signal = request.args.get("signal", "ALL")
 

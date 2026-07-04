710fbe1348d4fc105e60864b047046e940e99fc4
vkm10077 <vkm10077@gmail.com>
2026-07-04 12:17:00 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,12 +3,11 @@ from fyers_apiv3 import fyersModel
 
 def get_dashboard_data(client_id, access_token):
 
-    fyers = fyersModel.FyersModel(
-        client_id=str(client_id),
-        token=access_token,
-        is_async=False
-    )
-
+   fyers = fyersModel.FyersModel(
+    client_id=str(client_id),
+    token=f"{client_id}:{access_token}",
+    is_async=False
+) 
     market_symbols = [
         "NSE:SBIN-EQ"
         

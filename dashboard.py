3a519c4ea5eb168beaa8b59588680fb4799f303c
vkm10077 <vkm10077@gmail.com>
2026-07-04 12:36:35 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,20 +1,23 @@
 from fyers_apiv3 import fyersModel
 
-
 def get_dashboard_data(client_id, access_token):
-    
-    fyers = fyersModel.FyersModel(
-        client_id=str(client_id),
-        token=f"{client_id}:{access_token}",
-        is_async=False
-    )
-    
-    market_symbols = [
-        "NSE:SBIN-EQ"
-    ]
 
-    quotes = fyers.quotes({
-        "symbols": ",".join(market_symbols)
-    })
-
-    return quotes
+    return {
+        "s": "ok",
+        "d": [
+            {
+                "s": "ok",
+                "n": "NSE:SBIN-EQ",
+                "v": {
+                    "short_name": "SBIN",
+                    "lp": 800,
+                    "ch": 5,
+                    "chp": 0.65,
+                    "high_price": 805,
+                    "low_price": 795,
+                    "open_price": 798,
+                    "prev_close_price": 795
+                }
+            }
+        ]
+    }

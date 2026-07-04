fef9b3e9fc06fcf17ad8752a12ebafd7abb46fd5
vkm10077 <vkm10077@gmail.com>
2026-07-04 12:01:59 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ from fyers_apiv3 import fyersModel
 def get_dashboard_data(client_id, access_token):
 
     fyers = fyersModel.FyersModel(
-        client_id=client_id,
+        client_id=str(client_id),
         token=access_token,
         is_async=False
     )
@@ -15,6 +15,9 @@ def get_dashboard_data(client_id, access_token):
         "BSE:SENSEX-INDEX",
         
     ]
+
+    print("CLIENT_ID:", client_id)
+    print("ACCESS_TOKEN START:", access_token[:20])
 
     quotes = fyers.quotes({
         "symbols": ",".join(market_symbols)

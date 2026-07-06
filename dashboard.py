6735417047cabe801776414f53dc91582b4e2594
vkm10077 <vkm10077@gmail.com>
2026-07-06 10:12:32 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,14 +3,6 @@ from market import get_fyers
 def get_dashboard_data(client_id, access_token):
     fyers = get_fyers(client_id, access_token)
 
-    symbols = [
-        "NSE:NIFTY50-INDEX",
-        "NSE:NIFTYBANK-INDEX",
-        "BSE:SENSEX-INDEX"
-    ]
-
-    data = {
-        "symbols": ",".join(symbols)
-    }
-
-    return fyers.quotes(data)
+    return fyers.quotes({
+        "symbols": "NSE:NIFTY50-INDEX,NSE:NIFTYBANK-INDEX,BSE:SENSEX-INDEX"
+    })

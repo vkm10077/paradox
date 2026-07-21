fb505a704bfb05ac76a01168a0b511fd94cb450d
vkm10077 <vkm10077@gmail.com>
2026-07-21 17:17:27 +0530

    Update market.py

--- market.py
+++ market.py
@@ -1,9 +1,1 @@
-from fyers_apiv3 import fyersModel
 
-
-def get_fyers(client_id, access_token):
-    return fyersModel.FyersModel(
-        client_id=client_id,
-        token=access_token,
-        is_async=False
-    )
